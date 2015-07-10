c2613975609fd0a40e903bc5948001e7c12658b3
Javier Talavera Moya <javi.tm90@gmail.com>
2015-07-10 22:57:34 +0200

    Nuevo Proyecto

---
+++ Project Sausage/Assets/Scripts/Traps/TrapAttributtes.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapAttributtes : MonoBehaviour {
+	public int Hits;
+	public double Damage;
+	public int Coin;
+	public int Debuff;
+}
---
+++ Project Sausage/Assets/Scripts/Traps/TrapMaster.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapMaster : MonoBehaviour {
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Enemy") {
+			Debug.Log("Enemigo Dañado");
+		}
+	}
+
+
+}
