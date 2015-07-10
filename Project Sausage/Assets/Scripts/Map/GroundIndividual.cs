26f4b8bed483d2bb8e993237f326107733c4213f
Javier Talavera Moya <javi.tm90@gmail.com>
2015-07-11 01:23:24 +0200

    Escena principal
    Terreno puesto
    MouseOver en terreno

---
+++ Project Sausage/Assets/Scripts/Map/GroundIndividual.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundIndividual : MonoBehaviour {
+
+	public Material normal;
+	public Material selected;
+	public bool trapped = false;
+
+	void OnMouseEnter () {
+		GetComponent<MeshRenderer> ().material = selected;
+	}
+
+	void OnMouseExit () {
+		GetComponent<MeshRenderer> ().material = normal;
+	}
+}
