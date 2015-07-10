6ed572cd991523f746c1823dea8e7a4e7da7c3ec
Javier Talavera Moya <javi.tm90@gmail.com>
2015-07-11 00:00:33 +0200

    Traps
    Trampa 2 funcionando
    Materiales de prueba subidos

---
+++ Project Sausage/Assets/Scripts/Traps/DamageDebuff.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageDebuff : MonoBehaviour {
+	
+	public int damage = 10;
+	
+	public void Start () {
+		// BAJAR LA VELOCIDAD
+		Debug.Log ("DAÑO REALIZADO");
+		Destroy (this);
+	}
+}
---
+++ Project Sausage/Assets/Scripts/Traps/TrapFire.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapFire : TrapMaster {
+	
+	override
+	public void Execute (GameObject other) {
+		other.AddComponent<DamageDebuff> ();
+	}
+}
