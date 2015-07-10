66f8ff68eff4d535a53cf52815ea61dd1cce0c61
jmcarayol <josemanuelcarayol@gmail.com>
2015-07-10 23:30:57 +0200

    Stats
    - Player
    - Enemy

---
+++ Project Sausage/Assets/Scripts/Stats/EnemyStats.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStats : PlayerStats {
+
+	// Stats especificos de los enemigos
+	//public WayPoint wp;
+	public int Score;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Project Sausage/Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+	// Stats basicos
+	public int Health;
+	public int Speed;
+	public int Damage;
+	public int Coins;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
