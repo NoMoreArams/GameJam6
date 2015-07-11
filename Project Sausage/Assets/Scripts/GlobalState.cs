b20550920dd7f8067da15fa5530af75ce109eba6
Javier Talavera Moya <javi.tm90@gmail.com>
2015-07-11 23:36:56 +0200

    Oleadas generadas

--- Project Sausage/Assets/Scripts/GlobalState.cs
+++ Project Sausage/Assets/Scripts/GlobalState.cs
@@ -7,12 +7,13 @@ public class GlobalState : MonoBehaviour {
 	public static int round;
 	public static int score;
 	public static int lifes;
-	public static int coins = 100;
+	public static int coins = 10;
 	public float time = 5f;
 	private float time2Start;
 	public int nEnemies;
 	public float tEnemies;
 	public bool gameStarted = false;
+	public int incrementBetweenWaves;
 
 	public Text scoreText;
 	public GameObject time2StartText;
@@ -35,13 +36,19 @@ public class GlobalState : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
-		if (time2Start <= 0 && !gameStarted) {
+		if (time2Start <= 0) {
 			gameStarted = true;
 			GameObject.Find ("pr_InitialWayPoint").GetComponent<EnemyGeneration> ().StartWave (nEnemies, tEnemies);
 			time2StartText.SetActive(false);
+			time2Start = 20;
+			nEnemies += incrementBetweenWaves;
+		} else if (time2Start <= 10 && time2Start >= 0) { 
+			time2StartText.SetActive(true);
+			time2Start -= Time.fixedDeltaTime;
+			time2StartText.GetComponent<Text>().text = time2Start.ToString("F0");
 		} else {
 			time2Start -= Time.fixedDeltaTime;
-			time2StartText.GetComponent<Text>().text = time2Start.ToString("F0");;
+			time2StartText.GetComponent<Text>().text = time2Start.ToString("F0");
 		}
 		coinsText.text = coins.ToString ();
 		scoreText.text = score.ToString ();
