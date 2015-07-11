93d3d8449eb994015614160120588a2a2f5311ed
Javier Talavera Moya <javi.tm90@gmail.com>
2015-07-11 13:08:09 +0200

    TrapFuego

---
+++ Project Sausage/Assets/FogonazoHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogonazoHandler : MonoBehaviour {
+
+	public Light light;
+	
+	// Update is called once per frame
+	void Update () {
+		light.intensity -= 0.01f;
+		if (light.intensity <= 0f)
+			Destroy (gameObject);
+	}
+}
--- Project Sausage/Assets/Scripts/Traps/TrapFire.cs
+++ Project Sausage/Assets/Scripts/Traps/TrapFire.cs
@@ -2,9 +2,12 @@
 using System.Collections;
 
 public class TrapFire : TrapMaster {
-	
+
+	public GameObject fire;
+
 	override
 	public void Execute (GameObject other) {
+		Instantiate (fire, transform.position, fire.transform.rotation);
 		DamageDebuff buff = other.AddComponent<DamageDebuff> ();
 		buff.Execute (tAttr.damage);
 	}
