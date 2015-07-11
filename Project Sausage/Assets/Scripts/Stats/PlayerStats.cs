aba6105dcbb7dca8c80317f3aa50e1eeed11da99
jmcarayol <josemanuelcarayol@gmail.com>
2015-07-11 10:03:34 +0200

    Stats & PlayerDie
    Stats
    - Controlar muerte: Alive
    - ReceiveDamage
    
    PlayerDie
    - Muerte y resurrección

--- Project Sausage/Assets/Scripts/Stats/PlayerStats.cs
+++ Project Sausage/Assets/Scripts/Stats/PlayerStats.cs
@@ -4,20 +4,45 @@ using System.Collections;
 public class PlayerStats : MonoBehaviour {
 
 	// Stats basicos
+	public int MaxHealth;
 	public int Health;
 	public int Speed;
 	public int Damage;
 	public int Coins;
 
+	// Indicador de si esta vivo
+	private bool alive;
+
+	public bool Alive
+	{
+		get { return alive;}
+	}
+
 	// Use this for initialization
 	void Start ()
 	{
-
+		// Empezar vivo
+		alive = true;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	
+		// Comprobar si esta vivo
+		if (Health == 0)
+			alive = false;
+		else
+			alive = true;
+	}
+
+	// Recibir daño
+	public void ReceiveDamage(int damage)
+	{
+		// Restar vida
+		Health -= damage;
+
+		// Comprobar no sea menor de cero
+		if (Health < 0)
+			Health = 0;
 	}
 }
