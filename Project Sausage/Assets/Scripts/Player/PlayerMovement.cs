8c2a763cd2a025ae893ee9c55f52a6442adbc6f7
Javier Talavera Moya <javi.tm90@gmail.com>
2015-07-11 02:54:11 +0200

    Ajuste de tamaños

--- Project Sausage/Assets/Scripts/Player/PlayerMovement.cs
+++ Project Sausage/Assets/Scripts/Player/PlayerMovement.cs
@@ -32,7 +32,7 @@ public class PlayerMovement : MonoBehaviour {
 	void Update ()
 	{
 		// Recoger movimiento
-		Debug.Log ("h: " + h + "; v: " + v + "; camara_ry: " + camara.transform.rotation.x);
+		//Debug.Log ("h: " + h + "; v: " + v + "; camara_ry: " + camara.transform.rotation.x);
 
 		// Obtener velocidad
 		speed = gameObject.GetComponent<PlayerStats>().Speed;
