92d92f14d36fb1901904b0066e488fa662522bc0
isidurlll <isidurlll@gmail.com>
2015-07-11 18:10:30 +0200

    Hit Skill Enemy

--- Project Sausage/Assets/Scripts/Enemys/EnemyType3.cs
+++ Project Sausage/Assets/Scripts/Enemys/EnemyType3.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class EnemyType3 : EnemyBase
 {
+    private GameObject thrower;
+    public GameObject[] skills;
 
     protected override void Awake()
     {
         base.Awake();
+        thrower = transform.FindChild("Thrower").gameObject;
     }
     // Use this for initialization
     protected override void Start()
@@ -29,11 +32,40 @@ public class EnemyType3 : EnemyBase
 
     protected override void Movement()
     {
+        //StopCoroutine("ThrowSkill");
         base.Movement();
     }
 
     protected override void Attack()
     {
         base.Attack();
+    }
+
+    void ThrowSkill()
+    {
+        GameObject ob_shoot = Instantiate(skills[0], thrower.transform.position, Quaternion.identity) as GameObject;
+
+        HitController w_shootControler = ob_shoot.GetComponent<HitController>();
+        w_shootControler.SetEnemyDamage(enemyStats.Damage);
+    }
+    /*IEnumerator ThrowSkill()
+    {
+        // Realizar en animación
+        while (true)
+        {
+            GameObject ob_shoot = Instantiate(skills[0], thrower.transform.position, Quaternion.identity) as GameObject;
+            Vector3 w_direction = targetPlayer.transform.position - thrower.transform.position;
+            w_direction = w_direction / w_direction.magnitude;
+            ShootController w_shootControler = ob_shoot.GetComponent<ShootController>();
+            w_shootControler.SetPlayerTarget(w_direction);
+            w_shootControler.SetEnemyDamage(enemyStats.Damage);
+
+            yield return new WaitForSeconds(3.0f);
+        }
+    }*/
+
+    protected override void StarAttack()
+    {
+        ThrowSkill();
     }
 }
---
+++ Project Sausage/Assets/Scripts/Enemys/Skills/HitController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitController : MonoBehaviour {
+
+
+    private int damage;
+    private bool markToDestroy = false;
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(gameObject, 2.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (markToDestroy)
+            DestroyImmediate(gameObject);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            DamageDebuff db = other.gameObject.AddComponent<DamageDebuff>();
+            db.Execute(damage);
+            markToDestroy = true;
+        }
+    }
+
+    public void SetEnemyDamage(int pe_enemyDamage)
+    {
+        damage = pe_enemyDamage;
+    }
+}
