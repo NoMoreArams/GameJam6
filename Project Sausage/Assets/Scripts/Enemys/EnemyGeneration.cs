d97e1c1999c0fb60ecc3f78e3df22e1cda0ad871
isidurlll <isidurlll@gmail.com>
2015-07-11 10:13:04 +0200

    Generación de oleadas

--- Project Sausage/Assets/Scripts/Enemys/EnemyGeneration.cs
+++ Project Sausage/Assets/Scripts/Enemys/EnemyGeneration.cs
@@ -6,8 +6,8 @@ public class EnemyGeneration : MonoBehaviour {
     public GameObject[] enemy;
     public WayPoint[] initialsWayPoints;
 
-    public int numEnemys;
-    public float timeBetweenEnemys;
+    private int numEnemys;
+    private float timeBetweenEnemys;
 	// Use this for initialization
 	void Start () {
         
@@ -17,15 +17,31 @@ public class EnemyGeneration : MonoBehaviour {
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Space) && enemy.Length > 0)
         {
-            int w_enemy = (int)Random.Range(0, enemy.Length);
-            GameObject go_enemy = Instantiate(enemy[w_enemy], transform.position, Quaternion.identity) as GameObject;
-            int w_wayPoint = (int)Random.Range(0, initialsWayPoints.Length);
-            go_enemy.GetComponent<EnemyBase>().setWayPoint(initialsWayPoints[w_wayPoint]);
+            StartWave(5, 1.5f);
         }
 	}
 
     public void StartWave(int pe_numEnemys, float pe_timeBetweenEnemys)
     {
+        numEnemys = pe_numEnemys;
+        timeBetweenEnemys = pe_timeBetweenEnemys;
 
+        StartCoroutine("InitializeEnemys");
+    }
+
+    IEnumerator InitializeEnemys()
+    {
+        int w_enemy, w_wayPoint;
+        int w_contEnemys = 0;
+        while (w_contEnemys < numEnemys)
+        {
+            w_enemy = (int)Random.Range(0, enemy.Length);
+            GameObject go_enemy = Instantiate(enemy[w_enemy], transform.position, Quaternion.identity) as GameObject;
+            w_wayPoint = (int)Random.Range(0, initialsWayPoints.Length);
+            go_enemy.GetComponent<EnemyBase>().setWayPoint(initialsWayPoints[w_wayPoint]);
+            w_contEnemys++;
+
+            yield return new WaitForSeconds(timeBetweenEnemys);
+        }
     }
 }
