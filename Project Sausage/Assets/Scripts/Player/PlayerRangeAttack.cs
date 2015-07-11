ba569fa86b91915466b25aa1b9ba9bc311a2ac35
jmcarayol <josemanuelcarayol@gmail.com>
2015-07-11 12:02:10 +0200

    Attacks

---
+++ Project Sausage/Assets/Scripts/Player/PlayerRangeAttack.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRangeAttack : MonoBehaviour {
+
+	// Fuerza con la que se lanza el cuchillo
+	public float force;
+
+	// Velocidad del cuchillo
+	public float speed;
+
+	// Cuchillo
+	public GameObject knife;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// Click izquierdo
+		if (Input.GetMouseButtonDown(0))
+			ThrowKnife ();
+	}
+
+	// Lanzar cuchillo
+	void ThrowKnife ()
+	{
+		GameObject knifeClone = (GameObject)Instantiate(knife, transform.position, transform.rotation);
+		//knifeClone.velocity = transform.forward * speed;
+	}
+}
